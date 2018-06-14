5719173a5ea74247a4312283eb5ace37c3eaa94a
nikhilsharma2571 <39938550+nikhilsharma2571@users.noreply.github.com>
2018-06-15 01:34:36 +0530

    Assignment-2 completed

---
+++ Assignment-2.py
@@ -0,0 +1,25 @@
+#Q1.
+print("Ishika Garg")
+
+#Q2.
+print("acad"+"view")
+
+#Q3.
+a=input("Enter number 1=")
+b=input("Enter number 2=")
+c=input("Enter number 3=")
+print("a="+a+" b="+b+" c="+c)
+
+#Q4.
+print('Let\'s get started')
+
+#Q5.
+s="Acadview"
+course="Python"
+fees=5000
+print("hiii from %s, we are offerring %s course whose fees is %d"%(a,course,fees))
+
+#Q6.
+name="Tony Stark"
+salary=1000000
+print('%s"%d'%(name,salary))
