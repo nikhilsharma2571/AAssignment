4a24c26b9f4f8d5d1b665da2e93891b28b7b0e18
nikhilsharma2571 <39938550+nikhilsharma2571@users.noreply.github.com>
2018-06-15 01:28:19 +0530

    Assignment-3 completed

---
+++ Assignment-3.py
@@ -0,0 +1,34 @@
+#Q1.
+l=[]
+b=input("Enter number=")
+l.append(b)
+print(l)
+
+#Q2.
+l.append(['google','apple','facebook','microsoft','tesla'])
+print(l)
+
+#Q3.
+c=["apple","mango""strawberry","apple"]
+print(c.count("apple"))
+
+#Q4.
+ll=[4,3,1,5,3,2,34]
+ll.sort()
+print(ll)
+
+#Q5.
+A=[1,2,3]
+B=[2,3,4]
+C=A+B
+C.sort()
+print("Sorted merge list is -",C)
+
+#Q6.
+D=[2,1,3,4,2]
+print("Implementation of stack--")
+print(D)
+D.append(9)
+print(D)
+D.pop()
+print(D)
